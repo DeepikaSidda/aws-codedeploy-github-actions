2e404037637b1a0bd77cfe92c54763f67a02c820
Sidda Deepika <146612535+DeepikaSidda@users.noreply.github.com>
2025-07-18 11:50:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
+cat << EOF >app.py 
 
-@app.route('/')
-def hello():
-    return "Hello from EKS! Deployed across multiple regions using GitHub Actions and AWS!"
+from flask import Flask 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+app = Flask(__name__) 
+
+ 
+
+@app.route('/') 
+
+def demoapp(): 
+
+  return 'Hello from EKS! This application is built using Github Actions on AWS CodeBuild' 
+
+ 
+
+if __name__ == '__main__': 
+
+  app.run(port=8080,host='0.0.0.0') 
+
+EOF 
